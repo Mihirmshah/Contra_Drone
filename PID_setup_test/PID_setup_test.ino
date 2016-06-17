56a1427d34f7ce0db33d551813d0571e6e0496bc
Mihir <mihirmshah8@gmail.com>
2016-06-17 16:28:14 -0400

    finished initialization for three motors. Need to determine appropriate min/max values for output limits, as they aren't all going to PWM on arduino, but are passthroughs of RX inputs (1000-2000)

--- PID_setup_test/PID_setup_test.ino
+++ PID_setup_test/PID_setup_test.ino
@@ -17,13 +17,24 @@ double pid_m1_in, pid_m1_out, pid_m1_setpoint = 0;
 double pid_m2_in, pid_m2_out, pid_m2_setpoint = 0;
 double pid_pitch_in, pid_pitch_out, pid_pitch_setpoint = 0;
 
-PID m1PID(&pid_m1_in, &pid_m1_out, &pid_m1_setpoint, Kp_m1, Ki_m1, Kd_m1, DIRECT);
-PID m2PID(&pid_m2_in, &pid_m2_out, &pid_m2_setpoint, Kp_m2, Ki_m2, Kd_m2, DIRECT);
-PID pitchPID(&pid_pitch_in, &pid_pitch_out, &pid_pitch_setpoint, Kp_pitch, Ki_pitch, Kd_pitch, DIRECT);
+PID m1PID(&pid_m1_in, &pid_m1_out, &pid_m1_setpoint, Kp_m1, Ki_m1, Kd_m1, m1PID_direction);
+PID m2PID(&pid_m2_in, &pid_m2_out, &pid_m2_setpoint, Kp_m2, Ki_m2, Kd_m2, m2PID_direction);
+PID pitchPID(&pid_pitch_in, &pid_pitch_out, &pid_pitch_setpoint, Kp_pitch, Ki_pitch, Kd_pitch, pitchPID_direction);
 
+void pid_initialize () {
 
-SetMode(AUTOMATIC)
-
+  m1PID.SetMode(AUTOMATIC);
+  m1PID.SetOutputLimits(m1PID_MIN, m1PID_MAX);
+  m1PID.SetSampleTime(100);
+  
+  m2PID.SetMode(AUTOMATIC);
+  m2PID.SetOutputLimits(m2PID_MIN, m2PID_MAX);
+  m2PID.SetSampleTime(100);
+  
+  pitchPID.SetMode(AUTOMATIC);
+  pitchPID.SetOutputLimits(pitchPID_MIN, pitchPID_MAX);
+  pitchPID.SetSampleTime(100);
+}
 
 void setup() {
   // put your setup code here, to run once:
